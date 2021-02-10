e194cf9d16cf1d960d4eb81e141e5d6b2fb85bb8
883777 <68378672+883777@users.noreply.github.com>
2021-02-10 21:38:29 +0530

    Add files via upload

---
+++ TCPIP protocol communication/TCPIP protocol communication/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPIP_protocol_communication
+{
+    public partial class Form1 : Form
+    {
+        TcpClient clientsocket = new TcpClient();
+        NetworkStream serverStream = default(NetworkStream);
+        string readdata = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            clientsocket.Connect(textBox1.Text, Int32.Parse(textBox2.Text));
+
+            Thread ctThread = new Thread(getMessage);
+            ctThread.Start();
+        }
+
+        private void getMessage()
+        {
+            string returndata;
+           
+            while (true)
+            {
+                serverStream = clientsocket.GetStream();
+               var  buffsize = clientsocket.ReceiveBufferSize;
+                byte[] instream = new byte[buffsize];
+
+
+                serverStream.Read(instream, 0, buffsize);
+                returndata = System.Text.Encoding.ASCII.GetString(instream);
+                readdata = returndata;
+                msg();
+            }
+       
+        }
+
+        private void msg()
+        {
+            if(this.InvokeRequired)
+                
+             {
+                this.Invoke(new MethodInvoker(msg));
+            }
+            else
+            {
+                textBox4.Text = readdata;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] outstream = Encoding.ASCII.GetBytes(textBox3.Text);
+            
+            serverStream.Write(outstream, 0, outstream.Length);    
+            serverStream.Flush();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+} 
